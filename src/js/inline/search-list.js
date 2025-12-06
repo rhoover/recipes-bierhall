64585bbfa05d53e133b3d240dc9cac285579563c
rhoover <robin.hoover@gmail.com>
2025-12-06 12:47:49 -0500

    Finalizing

--- src/js/inline/search-list.js
+++ src/js/inline/search-list.js
@@ -1,41 +1,46 @@
 (() => {
 'use strict';
 
-  let listCards = document.querySelectorAll('.list-card');
-  let searchInput = document.getElementById('searchbox');
+  setTimeout(() => {
 
-  function liveSearch() {
-    let searchQuery = searchInput.value;
+    let listCards = document.querySelectorAll('.list-card');
+    let searchInput = document.getElementById('searchbox');
 
-    for (let i = 0; i < listCards.length; i++) {
-      
-      // if the text is within the card and matches the search query, remove class, otherwise add class
-      listCards[i].classList[listCards[i].innerText.toLowerCase().includes(searchQuery.toLowerCase())
-      ?
-      'remove'
-      :
-      'add']('is-hidden');
+    function liveSearch() {
+      let searchQuery = searchInput.value;
 
+      for (let i = 0; i < listCards.length; i++) {
+        
+        // if the text is within the card and matches the search query, remove class, otherwise add class
+        listCards[i].classList[listCards[i].innerText.toLowerCase().includes(searchQuery.toLowerCase())
+        ?
+        'remove'
+        :
+        'add']('is-hidden');
+
+      };
     };
-  };
 
-  // a little delay
-  let typingTimer;
-  let typeInterval = 250;
-  
-  searchInput.addEventListener('keyup', timerFunction);
-  function timerFunction() {
-    clearTimeout(typingTimer);
-    typingTimer = setTimeout(liveSearch, typeInterval);    
-  };
+    // a little delay
+    let typingTimer;
+    let typeInterval = 250;
+    
+    searchInput.addEventListener('keyup', timerFunction);
+    function timerFunction() {
+      clearTimeout(typingTimer);
+      typingTimer = setTimeout(liveSearch, typeInterval);    
+    };
 
-  // clearing the input box
-  searchInput.addEventListener('search', clearSearch);
-  function clearSearch() {
-    for (let i = 0; i < listCards.length; i++) {
-      listCards[i].classList.remove('is-hidden');
-    };    
-  };
+    // clearing the input box
+    searchInput.addEventListener('search', clearSearch);
+    function clearSearch() {
+      for (let i = 0; i < listCards.length; i++) {
+        listCards[i].classList.remove('is-hidden');
+      };    
+    };
+
+  }, 2000);
+
 })();
 
 //copied and tweaked from here: https://css-tricks.com/in-page-filtered-search-with-vanilla-javascript/
