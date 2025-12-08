ef6ed67c2a17948019110302322332dce5c2b37b
rhoover <robin.hoover@gmail.com>
2025-12-08 08:33:39 -0500

    Refactor

--- src/js/inline/login.js
+++ src/js/inline/login.js
@@ -1,40 +1,79 @@
 (() => {
   'use strict';
 
-  let openButton = document.querySelector('.home-login-button');
-  let closeButton = document.querySelector('.home-login-dialog-close');
-  let loginDialog = document.querySelector('.home-login-dialog');
+  const logInProgression = {
+    init() {
 
-  let mainElement = document.querySelector('.home');
+      // check and see if device is logged
+      let storedDevice = localStorage.getItem('storedDevice');
+      let logInButton = document.querySelector('.home-login-button');
 
-  let loginData;
+      if (storedDevice) {
+        // unlock the links
 
-  fetch('https://bierhallrecipes.com/creds/auth.json')
-    .then(response => response.json())
-    .then( (data) => {
-      loginData = data;
-    });
+        // modify login button
+        logInButton.disabled = true;
+        logInButton.innerHTML = 'Logged In Already!';
+        logInButton.classList.add('home-login-button-in');
+      } else {
+        // get the creds from the server
+        async function getCreds() {
+          let response = await fetch('https://bierhallrecipes.com/creds/auth.json');
+          let credsData = await response.json();
+          return credsData;
+        };
+        passThemForward();
+        async function passThemForward() {
+          let creds = await getCreds();
+          logInProgression.dealWithLogIn(creds, logInButton);
+        }
+      };
+    },
 
-  mainElement.addEventListener('click', (event) => {
+    dealWithLogIn(creds, logInButton) {
+      let logInObject = {};
+      let mainElement = document.querySelector('.home');  
+      let formInputs = document.querySelectorAll('input');
+      let loginDialog = document.querySelector('.home-login-dialog');
 
-    let whichButton = event.target.dataset.click;
+       mainElement.addEventListener('click', (event) => {
+        
+        let whichButton = event.target.dataset.click;
 
-    console.log(event.target);
+        switch (whichButton) {
+          case 'open':
+          loginDialog.showModal();            
+          break;
+          case 'close':
+            closingDialog();
+          break;
+          case 'dialog':
+            loginDialog.close();
+          break;
+          default:
+          break;
+        };
 
-    switch (whichButton) {
-      case 'open':
-        loginDialog.showModal();
-      break;
-      case 'close':
-        loginDialog.close();
-      break;
-      break;
-      case 'dialog':
-        loginDialog.close();
-      break;
-    
-      default:
-        break;
+        function closingDialog() {
+          event.preventDefault();
+          logInObject.email = formInputs[0].value;
+          logInObject.pw = formInputs[1].value;
+
+          if (JSON.stringify(creds) === JSON.stringify(logInObject)) {
+            let deviceLogged = {logged: true};
+            let storable = JSON.stringify(deviceLogged);
+
+            localStorage.setItem('storedDevice', storable);
+
+            logInButton.innerHTML = 'Log In Success!';
+            logInButton.classList.add('home-login-button-in');
+            logInButton.disabled = true;
+            
+            loginDialog.close();          
+          }; // end if
+        }; // end closingDialog
+       }); // end event listener
     }
-  });
+  };
+logInProgression.init();
 })();
