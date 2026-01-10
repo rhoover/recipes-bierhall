bb5e9695126deeba34f25766d76cd244a11f2245
rhoover <robin.hoover@gmail.com>
2026-01-10 10:51:12 -0500

    Dialog The Things

--- src/js/inline/recipe.js
+++ src/js/inline/recipe.js
@@ -1,71 +1,108 @@
 (() => {
   'use strict';
 
-  customElements.define('buttons-row', class extends HTMLElement {
+  // customElements.define('buttons-row', class extends HTMLElement {
 
-    connectedCallback() {
-      // initialize the things
-      this.buttonRow = document.querySelector('buttons-row');
-      this.buttonRowItems = this.buttonRow.querySelectorAll('.recipe-button');
-      this.addEventListener('click', this);
+  //   connectedCallback() {
+  //     // initialize the things
+  //     this.buttonRow = document.querySelector('buttons-row');
+  //     this.buttonRowItems = this.buttonRow.querySelectorAll('.recipe-button');
+  //     this.addEventListener('click', this);
       
-      // create data about what was clicked
-      this.addEventListener('click', (event) => {
-        const sourceButtonClicked = {
-          sourceData: event.target.getAttribute('data-source')
-        };
+  //     // create data about what was clicked
+  //     this.addEventListener('click', (event) => {
+  //       const sourceButtonClicked = {
+  //         sourceData: event.target.getAttribute('data-source')
+  //       };
 
-        // communicate with panels web component
-        this.dispatchEvent(new CustomEvent('toggle-panels', {
-          detail: sourceButtonClicked,
-          bubbles: true,
-          composed: true
-        }));
+  //       // communicate with panels web component
+  //       this.dispatchEvent(new CustomEvent('toggle-panels', {
+  //         detail: sourceButtonClicked,
+  //         bubbles: true,
+  //         composed: true
+  //       }));
 
-        // send it below for processing button classes
-        this.toggleButtonClasses(sourceButtonClicked);
-      }); // end eventListener
-    }; // end connectedCallback()
+  //       // send it below for processing button classes
+  //       this.toggleButtonClasses(sourceButtonClicked);
+  //     }); // end eventListener
+  //   }; // end connectedCallback()
 
-    toggleButtonClasses (sourceButtonClicked) {
-      this.buttonRowItems.forEach((button) => {
-        if (button.classList.contains('recipe-button-active')) {
-          button.classList.remove('recipe-button-active');
-        };
+  //   toggleButtonClasses (sourceButtonClicked) {
+  //     this.buttonRowItems.forEach((button) => {
+  //       if (button.classList.contains('recipe-button-active')) {
+  //         button.classList.remove('recipe-button-active');
+  //       };
 
-        if (button.getAttribute('data-source') === sourceButtonClicked.sourceData) {
-          button.classList.add('recipe-button-active');
-        };
-      }); // end foorEach
-    }; // end toggle buttons
-  }); // end customlements button-row
+  //       if (button.getAttribute('data-source') === sourceButtonClicked.sourceData) {
+  //         button.classList.add('recipe-button-active');
+  //       };
+  //     }); // end foorEach
+  //   }; // end toggle buttons
+  // }); // end customlements button-row
 
-  customElements.define('panels-row', class extends HTMLElement {
+  // customElements.define('panels-row', class extends HTMLElement {
 
-    connectedCallback() {
-      this.panels = this.querySelectorAll('.recipe-panel');
+  //   connectedCallback() {
+  //     this.panels = this.querySelectorAll('.recipe-panel');
 
-      // have to listen on the whole document to pick up this custom event
-      document.addEventListener('toggle-panels', (event) => {
-        // self-explanatory
-        document.startViewTransition(() => {
-          this.togglePanelClasses(event.detail);
-        });
-      });
-    }; // end connectedCallback()
+  //     // have to listen on the whole document to pick up this custom event
+  //     document.addEventListener('toggle-panels', (event) => {
+  //       // self-explanatory
+  //       document.startViewTransition(() => {
+  //         this.togglePanelClasses(event.detail);
+  //       });
+  //     });
+  //   }; // end connectedCallback()
 
-    // self-explanatory
-    togglePanelClasses (targetPanelData) {
-      this.panels.forEach((panel) =>{
-        if (panel.classList.contains('recipe-panel-active')) {
-          panel.classList.remove('recipe-panel-active');
-        };
+  //   // self-explanatory
+  //   togglePanelClasses (targetPanelData) {
+  //     this.panels.forEach((panel) =>{
+  //       if (panel.classList.contains('recipe-panel-active')) {
+  //         panel.classList.remove('recipe-panel-active');
+  //       };
 
-        if (panel.getAttribute('data-target') === targetPanelData.sourceData) {
-          panel.classList.add('recipe-panel-active');
-        };
-      }); // end forEach
-    }; // end togglePanelClasses
-  }); // end customElements panels-row
+  //       if (panel.getAttribute('data-target') === targetPanelData.sourceData) {
+  //         panel.classList.add('recipe-panel-active');
+  //       };
+  //     }); // end forEach
+  //   }; // end togglePanelClasses
+  // }); // end customElements panels-row
+
+  let mainElement = document.querySelector('.recipe');
+  let procedureDialog = document.querySelector('.recipe-dialog-procedure');
+  let notesDialog = document.querySelector('.recipe-dialog-notes');
+  let notesList = document.querySelectorAll('.recipe-note');
+
+
+    if (notesList.length == 0) {
+      nothingToSeeHere();
+    };
+    function nothingToSeeHere () {
+      let emptyStatement = `<p class="recipe-dialog-notes-nothing">There are no notes with this recipe.</p>`;
+      notesDialog.insertAdjacentHTML('beforeend', emptyStatement);
+    };
+
+  mainElement.addEventListener('click', (event) => {
+    let whichButton = event.target.dataset.source;
+
+    switch (whichButton) {
+      case 'procedure':
+        procedureDialog.showModal();
+      break;
+      case 'procedure-close':
+        procedureDialog.close();
+      break;
+    
+      case 'notes':
+        notesDialog.showModal();
+      break;
+      case 'notes-close':
+        notesDialog.close();
+      break;
+
+      default:
+      break;
+    };
+  });
 
 })();
